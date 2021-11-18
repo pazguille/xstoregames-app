8d47074b82eb7d65d2d33818f4fae6d0909fab1f
Guille Paz <pazguille@Guilles-MacBook-Pro.local>
2021-11-18 10:50:48 -0300

    Add news section.

--- src/js/templates.js
+++ src/js/templates.js
@@ -2,12 +2,16 @@ const IVA = 0.21;
 const IIBB = 0.02;
 const AFIP = 0.35;
 const PAISA = 0.08;
-const PAISB = 0.30;
-const taxes = (1+IVA+IIBB+AFIP+PAISA).toFixed(2);
+
+function toFixed(number, decimals) {
+  const x = Math.pow(10, Number(decimals) + 1);
+  return parseFloat((Number(number) + (1 / x)).toFixed(decimals));
+}
 
 function convert(price, dollar) {
   const usdPrice = (price / dollar);
-  return (usdPrice * dollar * taxes).toFixed(2);
+  const final = toFixed(usdPrice * dollar, 2) + toFixed(price * IVA, 2) + toFixed(price * IIBB, 2) + toFixed(price * AFIP, 2) + toFixed(price * PAISA, 2);
+  return final.toFixed(2);
 }
 
 const formatter = new Intl.NumberFormat('es-AR', {
@@ -36,62 +40,37 @@ export function gameListTemplate(section) {
 `);
 }
 
-export function gameCardNewTemplate(game) {
+export function gamePriceTemplate(game) {
   const off = Math.round((game.price.amount - game.price.deal)*100/game.price.amount);
   return (`
-<article class="game-preview-new">
-  <div>
-    <h3 class="game-title"><a id="detail-${game.id}" href="./?id=${game.id}" class="link">${game.title}</a></h3>
-    <p class="game-by">by ${game.developer || game.publisher}</p>
-    ${game.game_pass ? `<img class="game-pass" src="./src/assets/game-pass.png" width="60px" height="11px" alt="Disponible en Game Pass">` : ''}
-    <div class="game-price">
-      ${off > 0 ? `<span class="game-price-off">${off}% OFF</span>` : ''}
-      <span class="game-price-amount">
-        ${game.price.deal > 0 ?
-          `🇦🇷 ${formatter.format(convert(game.price.deal, dollar))}`
-          : 'Gratis'
-        }
-      </span>
-      ${game.price.deal > 0 ?
-        `<small class="game-price-taxes">impuestos incluídos</small>`
-        : ''
-      }
-    </div>
-  </div>
-  <img class="game-img" width="315px" height="177px" decoding="async" alt="" src="${game.images.superheroart.url || game.images.titledheroart.url || game.images.titledheroart[0].url}?w=630">
-</article>
-`);
+<div class="game-price">
+  ${off > 0 ? `<span class="game-price-off">${off}% OFF</span>` : ''}
+  <span class="game-price-amount">
+    ${game.price.deal > 0 ?
+      `🇦🇷 ${formatter.format(convert(game.price.deal, dollar))}`
+      : 'Gratis'
+    }
+  </span>
+  ${game.price.deal > 0 ?
+    `<small class="game-price-taxes">impuestos incluídos</small>`
+    : ''
+  }
+</div>
+  `);
 }
 
-export function gameCardTemplate(game) {
-  const off = Math.round((game.price.amount - game.price.deal)*100/game.price.amount);
+export function gameInfoTemplate(game) {
   return (`
-<article class="game-preview">
-  <div>
-    <h3 class="game-title"><a id="detail-${game.id}" href="./?id=${game.id}" class="link">${game.title}</a></h3>
-    <p class="game-by">by ${game.developer || game.publisher}</p>
-    ${game.game_pass ? `<img class="game-pass" src="./src/assets/game-pass.png" width="60px" height="11px" alt="Disponible en Game Pass">` : ''}
-    <div class="game-price">
-      ${off > 0 ? `<span class="game-price-off">${off}% OFF</span>` : ''}
-      <span class="game-price-amount">
-        ${game.price.deal > 0 ?
-          `🇦🇷 ${formatter.format(convert(game.price.deal, dollar))}`
-          : 'Gratis'
-        }
-      </span>
-      ${game.price.deal > 0 ?
-        `<small class="game-price-taxes">impuestos incluídos</small>`
-        : ''
-      }
-    </div>
-  </div>
-  <img class="game-img" width="155px" decoding="async" loading="lazy" height="155px" alt="" src="${game.images.boxart.url}?w=310">
-</article>
-`);
+<div>
+  <h3 class="game-title"><a id="detail-${game.id}" href="./?id=${game.id}" class="link">${game.title}</a></h3>
+  <p class="game-by">by ${game.developer || game.publisher}</p>
+  ${game.game_pass ? `<img class="game-pass" src="./src/assets/game-pass.png" width="60px" height="11px" alt="Disponible en Game Pass">` : ''}
+  ${gamePriceTemplate(game)}
+</div>
+  `);
 }
 
 export function gameDeailTemplate(game) {
-  const off = Math.round((game.price.amount - game.price.deal)*100/game.price.amount);
   return (`
 <article class="game-preview" style="background-image: url(${game.images.superheroart.url || game.images.titledheroart.url || game.images.titledheroart[0].url}?w=1000)">
   <div>
@@ -99,28 +78,44 @@ export function gameDeailTemplate(game) {
       <h3 class="game-title">${game.title}</h3>
       <p class="game-by">by ${game.developer || game.publisher}</p>
       ${game.game_pass ? `<img class="game-pass" src="./src/assets/game-pass.png" width="70px" height="13px" alt="Disponible en Game Pass">` : ''}
-      <div class="game-price">
-        ${off > 0 ? `<span class="game-price-off">${off}% OFF</span>` : ''}
-        <span class="game-price-amount">
-          ${game.price.deal > 0 ?
-            `🇦🇷 ${formatter.format(convert(game.price.deal, dollar))}`
-            : 'Gratis'
-          }
-        </span>
-        ${game.price.deal > 0 ?
-          `<small class="game-price-taxes">impuestos incluídos</small>`
-          : ''
-        }
-      </div>
-      <a href="https://www.xbox.com/es-ar/games/store/a/${game.id}" class="game-buy-now btn">Comprar Ahora</a>
+      ${gamePriceTemplate(game)}
+      <a href="https://www.xbox.com/es-ar/games/store/a/${game.id}" class="game-buy-now btn">Comprar ahora</a>
       <p class="game-description">${game.description}</p>
     </div>
-    ${game.images.screenshot ? `
+    ${Array.isArray(game.images.screenshot) ? `
       <div class="game-preview-images">
         ${game.images.screenshot.map((img) => `<img width="100%" loading="lazy" decoding="async" src="${img.url}?w=1000" />`).join('')}
       </div>
     ` : ''}
   </div>
+</article>
+`);
+}
+
+export function gameCardNewTemplate(game) {
+  return (`
+<article class="game-preview-new">
+  ${gameInfoTemplate(game)}
+  <img class="game-img" width="315px" height="177px" alt="" decoding="async" src="${game.images.superheroart.url || game.images.titledheroart.url || game.images.titledheroart[0].url}?w=630">
+</article>
+`);
+}
+
+export function gameCardTemplate(game) {
+  return (`
+<article class="game-preview">
+  ${gameInfoTemplate(game)}
+  <img class="game-img" width="155px" height="155px" alt="" decoding="async" loading="lazy" src="${game.images.boxart.url}?w=310">
+</article>
+`);
+}
+
+export function newsTemplate(news) {
+  return (`
+<article class="news-preview">
+  <h2><a href="${news.link}">${news.title}</a></h2>
+  <img class="news-img" width="100%" height="auto" alt="" decoding="async" loading="lazy" src="${news.image}">
+  <p>${news.description}</p>
 </article>
 `);
 }
