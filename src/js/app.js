b600b46c936921823ff2bf8b5a7097e05b882713
Guille Paz <pazguille@Guilles-MacBook-Pro.local>
2021-11-26 22:35:59 -0300

    Make urls great again

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,7 @@ import {
 const LIMIT = 10;
 const getXboxURL = (list, skipitems = 0) => `https://api.xstoregames.com/api/games?list=${list}&skipitems=${skipitems}`;
 const searchXboxURL = (query) => `https://api.xstoregames.com/api/search?q=${query}`;
+const gameXboxURL = (id) => `https://api.xstoregames.com/api/games?id=${id}`;
 const getXboxNewsURL = () => `https://api.xstoregames.com/api/news`;
 // const getXboxURL = (list, skipitems = 0) => `http://localhost:3031/api/games?list=${list}&skipitems=${skipitems}`;
 // const searchXboxURL = (query) => `http://localhost:3031/api/search?q=${query}`;
@@ -145,67 +146,13 @@ export default async function bootApp() {
 
   document.body.addEventListener('click', async (eve) => {
     if (!eve.target.classList.contains('link')) { return; }
-
     eve.preventDefault();
     const data = eve.target.id.split('-');
     const type = data[0];
     const id = data[1];
-
+    showPage(type, id);
     $pageBack.removeAttribute('hidden');
-    $main.style.overflow = 'hidden';
-    document.body.style.overflow = 'hidden';
-
-    if (type === 'detail') {
-      $currentPage = $detail;
-      $currentPageContent = $detailContent;
-      const game = gamesCache.get(id);
-      const html = gameDetailTemplate(game);
-      requestIdleCallback(() => {
-        $currentPageContent.innerHTML = html;
-      });
-    }
-
-    if (type === 'collection') {
-      $currentPage = $list;
-      $currentPageContent = $listContent;
-
-      const section = sections.find(section => section.type === id);
-      section.list.map((game) => requestIdleCallback(() => {
-        $currentPageContent.insertAdjacentHTML('beforeend', gameCardTemplate(game));
-        gamesCache.set(game.id, game);
-      }));
-
-      requestIdleCallback(() => {
-        const o = new IntersectionObserver(async (entries) => {
-          const first = entries[0];
-          if (first.isIntersecting) {
-            o.unobserve(o.current);
-            const moreGames = await fetch(getXboxURL(id, section.skipitems += LIMIT)).then(res => res.json());
-            if (moreGames.length === 0) { return; }
-            moreGames.map((game) => requestIdleCallback(() => {
-              $currentPageContent.insertAdjacentHTML('beforeend', gameCardTemplate(game));
-              gamesCache.set(game.id, game);
-            }));
-            requestIdleCallback(() => {
-              o.current = $currentPageContent.lastElementChild;
-              o.observe(o.current);
-              section.list.push(...moreGames);
-            });
-          }
-        });
-        o.current = $currentPageContent.lastElementChild;
-        o.observe(o.current);
-      });
-    }
-
-    requestIdleCallback(() => {
-      $search.setAttribute('hidden', true);
-      $searchBtn.setAttribute('hidden', true);
-    });
-
     history.pushState({ page: type }, '', eve.target.href);
-    $currentPage.classList.add('page-on');
-    $pullToRefresh = $currentPageContent;
   });
 
   $pageBack.addEventListener('click', () => {
@@ -240,7 +187,7 @@ export default async function bootApp() {
       $currentPageContent = $listContent;
     }
 
-    if (eve.state && eve.state.page === 'deatil') {
+    if (eve.state && eve.state.page === 'detail') {
       $currentPage = $detail;
       $currentPageContent = $detailContent;
     }
@@ -324,16 +271,97 @@ export default async function bootApp() {
 
   $search.addEventListener('submit', async (eve) => {
     eve.preventDefault();
+    const q = eve.target.elements[0].value;
+    loadSearchPage(q);
+  });
 
+  async function showPage(type, id) {
+    $main.style.overflow = 'hidden';
+    document.body.style.overflow = 'hidden';
+
+    if (type === 'detail') {
+      $currentPage = $detail;
+      $currentPageContent = $detailContent;
+      let game = gamesCache.get(id);
+
+      if (!game) {
+        game = await fetch(gameXboxURL(id)).then(res => res.json()).then(game => game[0]);
+        gamesCache.set(game.id, game);
+      }
+
+      const html = gameDetailTemplate(game);
+      requestIdleCallback(() => {
+        $currentPageContent.innerHTML = html;
+      });
+    }
+
+    if (type === 'collection') {
+      $currentPage = $list;
+      $currentPageContent = $listContent;
+
+      const section = sections.find(section => section.type === id);
+      section.list.map((game) => requestIdleCallback(() => {
+        $currentPageContent.insertAdjacentHTML('beforeend', gameCardTemplate(game));
+        gamesCache.set(game.id, game);
+      }));
+
+      requestIdleCallback(() => {
+        const o = new IntersectionObserver(async (entries) => {
+          const first = entries[0];
+          if (first.isIntersecting) {
+            o.unobserve(o.current);
+            const moreGames = await fetch(getXboxURL(id, section.skipitems += LIMIT)).then(res => res.json());
+            if (moreGames.length === 0) { return; }
+            moreGames.map((game) => requestIdleCallback(() => {
+              $currentPageContent.insertAdjacentHTML('beforeend', gameCardTemplate(game));
+              gamesCache.set(game.id, game);
+            }));
+            requestIdleCallback(() => {
+              o.current = $currentPageContent.lastElementChild;
+              o.observe(o.current);
+              section.list.push(...moreGames);
+            });
+          }
+        });
+        o.current = $currentPageContent.lastElementChild;
+        o.observe(o.current);
+      });
+    }
+
+    requestIdleCallback(() => {
+      $search.setAttribute('hidden', true);
+      $searchBtn.setAttribute('hidden', true);
+    });
+
+    $currentPage.classList.add('page-on');
+    $pullToRefresh = $currentPageContent;
+  }
+
+  function loadHomePage() {
+    sections.forEach(section => {
+      requestIdleCallback(() => {
+        $home.insertAdjacentHTML('beforeend', sectionTemplate(section));
+      });
+    });
+  }
+
+  async function loadCollectionPage(list) {
+    showPage('collection', list);
+  }
+
+  async function loadDetailPage(id) {
+    showPage('detail', id);
+  }
+
+  async function loadSearchPage(q) {
     $currentPage = $results
     $currentPageContent = $resultsContent;
     $pullToRefresh = $currentPageContent;
 
-    const q = eve.target.elements[0].value;
     if (history.state === null) {
-      history.pushState({ page: 'results' }, 'Resultados de busqueda', `${eve.target.action}?q=${q}`);
+      history.pushState({ page: 'results' }, 'Resultados de busqueda', `/search?q=${q}`);
     } else {
-      history.replaceState({ page: 'results' }, 'Resultados de busqueda', `${eve.target.action}?q=${q}`);
+      history.replaceState({ page: 'results' }, 'Resultados de busqueda', `/search?q=${q}`);
     }
     $currentPage.classList.add('page-on');
 
@@ -350,7 +378,7 @@ export default async function bootApp() {
       $currentPageContent.insertAdjacentHTML('beforeend', gameCardTemplate(game));
       gamesCache.set(game.id, game);
     }));
-  });
+  }
 
   await Promise.all(sections.map(async ({type}) => {
     const games = await fetch(getXboxURL(type)).then(res => res.json());
@@ -359,13 +387,28 @@ export default async function bootApp() {
     games.forEach((game) => gamesCache.set(game.id, game));
   }));
 
+  const { pathname, searchParams } = new URL(window.location.href);
+  const pathSplit = pathname.split('/');
+  const type = pathSplit[1];
+  const id = pathSplit[2];
+
+  switch (type) {
+    case '':
+      loadHomePage();
+      break;
+    case 'game':
+      loadDetailPage(id);
+      break;
+    case 'collection':
+      loadCollectionPage(id);
+      break;
+    case 'search':
+      const q = searchParams.get('q');
+      loadSearchPage(q);
+      break;
+  }
+
   requestIdleCallback(() => {
     $loading.setAttribute('hidden', true);
-  });
-
-  sections.forEach(section => {
-    requestIdleCallback(() => {
-      $home.insertAdjacentHTML('beforeend', sectionTemplate(section));
-    });
   });
 }
